1f44aa40d8c914d424672fe477b46cd6b5578f05
丸山真麻 <126678249+Mao-75@users.noreply.github.com>
2024-02-23 13:49:10 +0900

    Update: methods.rb

--- Introduction_to_Ruby/methods.rb
+++ Introduction_to_Ruby/methods.rb
@@ -1,5 +1,29 @@
+メソッドの定義の基本
+→Rubyではメソッドを呼び出す際に引数に過不足があるとエラーになる
+
+def greet(country)
+  if country == 'japan'
+    'こんにちは'
+  else
+    'Hello'
+  end
+end
+
+[例] 引数がちょうど
+greet('us') #=> "Hello"
+
+[例] 引数が足りない
+# arguments(引数), given(与えられた引数の数), expected(期待している引数の数)の意味
+greet 
+#=> wrong number of arguments (given 0, expected 1) (ArgumentError)
+
+[例] 引数が多い
+greet('japan', 'us') 
+#=> wrong number of arguments (given 2, expected 1) (ArgumentError)
+
+
 メソッドの戻り値
-->returnメソッドはメソッドを途中で抜け出す場合に使われることが多い
+→returnメソッドはメソッドを途中で抜け出す場合に使われることが多い
 
 [例]
 def greeting(country)  # (引数)は仮の引数名なのでメソッドを呼び出す際に違う名前の引数でも処理は動く
@@ -20,7 +44,7 @@ greeting('japan') #=> 'こんにちは'
 
 
 sprintfメソッド
-->指定されたフォーマットの文字列を作成するメソッド
+→指定されたフォーマットの文字列を作成するメソッド
 
 [例1] 1.2の小数第３位まで数字を表示する文字列を作成
 sprintf('%0.3f', 1.2) #=> "1.200"
