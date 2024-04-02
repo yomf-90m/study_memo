e18bcc8bad324d8cc58d56a2b40008d05ca684f4
丸山真麻 <126678249+Mao-75@users.noreply.github.com>
2024-04-02 18:39:40 +0900

    Update: Repetition_processing.rb

--- Introduction_to_Ruby/Repetition_processing.rb
+++ Introduction_to_Ruby/Repetition_processing.rb
@@ -22,3 +22,23 @@ for n in numbers
   sum += n
 end
 sum #=> 10
+
+
+
+for文とeachメソッドの繰り返し処理の違い
+→for文は配列の要素を受け取る変数や、for文の中で作成したローカル変数がfor文の外でも使える。
+[例] for文の場合
+numbers = [1, 2, 3, 4]
+sum = 0
+for n in numbers
+  sum_value = n.even? ? n * 10 : n
+  sum += sum_value
+end
+
+# n はループが終了したときに4（numbersの最後の要素）の値を持ち、
+# sum_value は最後の要素4が偶数であるために 4 * 10 すなわち 40 が代入される
+# 従って、n の値は 4、sum_value の値は 40 となる
+n         #=> 4
+sum_value #=> 40
+
+→eachメソッドはブロックパラメータやブロック内で作成した変数をブロックの外では参照できない。
