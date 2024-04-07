4424c4c7e0409d460f4fe834f7510b3e967357c7
丸山真麻 <126678249+Mao-75@users.noreply.github.com>
2024-04-07 14:06:23 +0900

    Update: methods.rb

--- Introduction_to_Ruby/methods.rb
+++ Introduction_to_Ruby/methods.rb
@@ -155,3 +155,13 @@ numbers = [1, 2, 3, 4, 5]
 new_numbers = numbers.map { |n| n * 10 }
 
 new_numbers #=> [10, 20, 30, 40, 50]
+
+
+
+selectメソッド
+→selectメソッド(エイリアスメソッドはfind_all)は各要素に対してブロックを評価し、その戻り値が真の要素を集めた配列を返すメソッド。
+[例]
+numbers = [1, 2, 3, 4, 5, 6]
+# ブロックの戻り値が真になった要素だけが集められる
+even_numbers = numbers.select { |n| n.even? }
+even_numbers #=> [2, 4, 6]
