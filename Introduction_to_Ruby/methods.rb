bb773d0a11ea1d6d891775159e0b6e29b0d2e6e2
丸山真麻 <126678249+Mao-75@users.noreply.github.com>
2024-04-08 01:08:32 +0900

    Update: methods.rb

--- Introduction_to_Ruby/methods.rb
+++ Introduction_to_Ruby/methods.rb
@@ -165,3 +165,13 @@ numbers = [1, 2, 3, 4, 5, 6]
 # ブロックの戻り値が真になった要素だけが集められる
 even_numbers = numbers.select { |n| n.even? }
 even_numbers #=> [2, 4, 6]
+
+
+
+rejectメソッド
+→rejectメソッドはselectメソッドの反対で、ブロックの戻り値が真になった要素を除外した配列を返す、ブロックの戻り値が偽である要素を集めるメソッド。
+[例]
+numbers = [1, 2, 3, 4, 5, 6]
+# 3の倍数を除外する(３の倍数以外を集める)
+non_multiples_of_three = numbers.reject { |n| n % 3 == 0 }
+non_multiples_of_three #=> [2, 4, 6]
